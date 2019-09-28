7ea9698daf1aa004cf4ff7e0f0b1b63dee0443d8
thautwarm <twshere@outlook.com>
2019-09-28 17:18:48 +0800

    add more tests

--- test/test_hm.py
+++ test/test_hm.py
@@ -2,16 +2,36 @@ from rsolve.hm.unification import TCEnv, HMUnify
 from rsolve.hm.hm import *
 from rsolve.hm.nominal import *
 
+# t1 == t2 && t1 == int32
+# =>
+# t1 = t2 = int32
 env = TCEnv(None)
-
 t1 = env.new_tvar()
 t2 = env.new_tvar()
-
 eq = HMUnify(t1, t2)
 inst = HMUnify(t1, TNom(Int(32)))
-
 env.unify(eq)
-
 env.unify(inst)
+assert env.prune(t2) == TNom(Int(32))
 
-assert env.prune(t1) == TNom(Int(32))
+# t1 == (t2, t2) && t1 == t3 && t3 == (text32, t4)
+# =>
+# t1 = t3 = (text32, text32)
+# t2 = t4 = text32
+text32 = TNom(Text(32))
+env = TCEnv(None)
+t1 = env.new_tvar()
+t2 = env.new_tvar()
+t3 = env.new_tvar()
+t4 = env.new_tvar()
+eq1 = HMUnify(t1, TTup((t2, t2)))
+eq2 = HMUnify(t1, t3)
+eq3 = HMUnify(t3, TTup((text32, t4)))
+
+for eq in [eq1, eq2, eq3]:
+    env.unify(eq)
+
+assert env.prune(t1) == TTup((text32, text32))
+assert env.prune(t2) == text32
+assert env.prune(t3) == TTup((text32, text32))
+assert env.prune(t4) == text32
